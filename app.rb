9a75232d1aa6a5e2a850f7b935d4ef8a6c277840
Abigail Slovick <abby.slovick@gmail.com>
2018-07-31 15:41:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,16 +1,36 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/favorite-food' do
+    "My favorite food is ice cream! I especially enjoy frozen custard."
+  end 
+  get '/name' do 
+    "My name is Abby."
+  end
+  get '/hometown' do
+    "My hometown is Kansas City."
+  end
+  get '/favorite-song' do
+    "My favorite song is Born Bob Dylan by the Veronicas"
+  end 
+end 
+  
+    
+    
+    
+    
+    
+    
+    
+#   "My name is Abby,"
+#   end
+#   get '/hometown' do
+#   "My hometown is Kansas City."
+#   end
+#   get '/favorite-song' do
+#   "My favorite song is blank."
+#   end
+# end
   get '/' do
   "Hello, World!"
   end
-  get '/name' do
-  "My name is Abby,"
-  end
-  get '/hometown' do
-  "My hometown is Kansas City."
-  end
-  get '/favorite-song' do
-  "My favorite song is blank."
-  end
-end
